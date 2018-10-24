b734619f5b35145a9081f94bcd9458f9d9cf618e
Beazar <sander.beazar.y2288@student.hogent.be>
2018-10-24 17:05:22 +0200

    eerste activiteiten en layout detailpagina aanmaken

---
+++ app/src/main/java/com/example/sanderbeazar/sportinaalst/SportclubDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.example.sanderbeazar.sportinaalst
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SportclubDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sportclub_detail)
+
+    }
+}
---
+++ app/src/main/java/com/example/sanderbeazar/sportinaalst/domain/Sportclub.kt
@@ -0,0 +1,9 @@
+package com.example.sanderbeazar.sportinaalst.domain
+
+import java.io.Serializable
+
+class Sportclub (val naam: String = "", val sport: String= "", val email: String= "",
+                 val website: String="", val jongen: Boolean = true, val meisje: Boolean = true,
+                 val Postcode : Int, val adres : String = "", val Id : String): Serializable{
+
+}
